f636f1a6eefb1d7c797cbb0709b841e8f1bd9fdd
Ayush <Ayushbro779@gmail.com>
2026-06-29 08:54:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Number of factors/number-of-factors.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countFactors(self, n):
+        count = 0
+        
+        i = 1
+        while i * i <= n:
+            if n % i == 0:
+                count += 1
+                
+                if i != n // i:
+                    count += 1
+                    
+            i += 1
+            
+        return count
